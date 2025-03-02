29851d7dab31438724819bb9abaf680b32a44bcf
DhineshPS <dhineshselva411@gmail.com>
2025-03-02 12:04:52 +0530

    commit

--- ZohoJava/src/zoho/java/task2/Program2.java
+++ ZohoJava/src/zoho/java/task2/Program2.java
@@ -14,7 +14,7 @@ public class Program2 {
 		bitwiseXor = num1 ^ num2;
 		leftShift = num1 >> 2;
 		rightShift = num2 << 2;
-		urightShift = num1 >>> 2;
+		unrightShift = num1 >>> 2;
 		compliment = (~num1);
 		
 		System.out.println("Bitwise And : " + bitwiseAnd + " Bit of answer is 1 if both num1 and num2 bits are 1, else 0");
@@ -22,7 +22,7 @@ public class Program2 {
 		System.out.println("Bitwise Xor : " + bitwiseXor + " Bit of answer is 1 if both num1 and num2 bits are not equal, else 0");
 		System.out.println("Left shift : " + leftShift + " Shifting the binary bits of the number to left by n times");
 		System.out.println("Right shift : " + rightShift + " Shifting the binary bits of the number to right by n times");
-		System.out.println("Unsigned Right shift : " + urightShift + " Shifting the binary bits of the number to right by n times and left filled with 0's");
+		System.out.println("Unsigned Right shift : " + unrightShift + " Shifting the binary bits of the number to right by n times and left filled with 0's");
 		System.out.println("Compliment : " + compliment + " Toggling of bits - 0 to 1 and 1 to 0, and answer is 2's complimented(-(n + 1))");
 	}
 
